1d182a375421215a3cbdcbd25346459b86f79a4f
Will-h0219 <wherrera0219@gmail.com>
2021-11-03 12:37:34 -0500

    spinner, paginator & snackbar

--- src/app/modules/home/pages/search/search.component.ts
+++ src/app/modules/home/pages/search/search.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Character } from '../../../../data/characters/interfaces/characters.interface';
-import { SearchParams } from "../../../../data/characters/interfaces/search.interface";
+import { SearchParams } from '../../../../data/characters/interfaces/search.interface';
 import { CharactersService } from '../../../../data/characters/services/characters.service';
+import { PageEvent } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-search',
@@ -10,23 +12,51 @@ import { CharactersService } from '../../../../data/characters/services/characte
 })
 export class SearchComponent implements OnInit {
 
+  searchParams!: SearchParams;
   characters: Character[] = [];
+  displaySpinner: boolean = false;
+  displayPaginator: boolean = false;
+  paginatorLength: number = 0;
 
-  constructor(private charactersService: CharactersService) { }
+  constructor(
+    private charactersService: CharactersService,
+    private snackBar: MatSnackBar
+    ) { }
 
   ngOnInit(): void {
   }
 
   buscar(searchParams: SearchParams) {
-    let queryString = Object.keys(searchParams).map((key) => key + '=' + searchParams[key]).join('&');
     if (Object.keys(searchParams).length === 0) {
-      console.log('hey')
+      this.snackBar.open('You must enter at least one search parameter.', 'Ok')
     } else {
+      this.searchParams = searchParams;
+      let queryString = Object.keys(searchParams).map((key) => key + '=' + searchParams[key]).join('&');
+      this.displaySpinner = true;
       this.charactersService.filteredCharacters(queryString)
         .subscribe(resp => {
-          this.characters = resp.result;
+          if (resp.info.count === 0) {
+            this.snackBar.open("There's no results for this search", "Ok");
+            this.displaySpinner = false;
+          } else {
+            this.characters = resp.result;
+            this.paginatorLength = resp.info.count;
+            this.displayPaginator = (resp.info.pages > 1);
+            this.displaySpinner = false;
+          }
         })
     }
+  }
+
+  getPagination(pageEvent: PageEvent) {
+    let parameters = Object.keys(this.searchParams).map((key) => key + '=' + this.searchParams[key]).join('&');
+    let queryString = `page=${pageEvent.pageIndex + 1}&${parameters}`;
+    this.displaySpinner = true;
+    this.charactersService.filteredCharacters(queryString)
+      .subscribe(resp => {
+        this.characters = resp.result;
+        this.displaySpinner = false;
+      })
   }
 
 }
